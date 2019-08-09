71f60c4c99c1694a0bdda266131b74f7682599ef
Nhung Hoang <nhung@nhunghoang.com>
2019-08-09 12:41:29 +1000

    added Bookmark

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
+  has_many :bookmarks
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
